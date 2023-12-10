d24cf69f44d68eea3ae29229cdb9a3e1f24e4870
Marco Chavez <marcochavez1940@gmail.com>
2023-12-10 15:35:29 -0600

    add switch components to select the units of temperature

--- src/components/ListWeather.js
+++ src/components/ListWeather.js
@@ -2,21 +2,30 @@ import { useEffect, useState } from "react";
 import { getWeather } from "../services/weather.api";
 import CityCard from "./CityCard";
 
-import { Space } from 'antd';
+import { Space, Switch } from 'antd';
 
 const ListWeather = ({ name, lat, lon }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [units, setUnits] = useState('metric');
   const [weather, setWeather] = useState([])
 
   useEffect(() => {
     setIsLoading(true);
     async function fetchData() {
-      const result = await getWeather({ lat, lon });
+      const result = await getWeather({ lat, lon, units });
       setWeather(result)
       setIsLoading(false);
     }
     fetchData();
-  }, [name])
+  }, [name, units]);
+
+  const changeUnits = (checked) => {
+    if (checked) {
+      setUnits('metric')
+    } else {
+      setUnits('imperial');
+    }
+  };
 
   if (isLoading)
     return <div style={{ color: 'white' }}>Loading...</div>
@@ -25,21 +34,30 @@ const ListWeather = ({ name, lat, lon }) => {
     return <div style={{ color: 'white' }}>There is not result</div>
 
   return (
-    <Space direction="vertical" size={16} style={{ width: '100%' }}>
-      {weather.map((forecast, i) => {
-        return (
-          <CityCard
-            key={i}
-            day={forecast.dt}
-            tempMin={forecast.main.temp_min}
-            temp={forecast.main.temp}
-            tempMax={forecast.main.temp_min}
-            weather={forecast.weather[0].description}
-            weatherImg={forecast.weather[0].icon}
-          />
-        )
-      })}
-    </Space>
+    <div>
+      <Space size={8}>
+        <p style={{ color: 'white' }}>F°</p>
+        <Switch checked={units === 'metric'} onChange={changeUnits} style={{ backgroundColor: units === 'metric' ? 'green' : 'blue' }} />
+        <p style={{ color: 'white' }}>C°</p>
+      </Space>
+
+      <Space direction="vertical" size={16} style={{ width: '100%' }}>
+        {weather.map((forecast, i) => {
+          return (
+            <CityCard
+              key={i}
+              day={forecast.dt}
+              tempMin={forecast.main.temp_min}
+              temp={forecast.main.temp}
+              tempMax={forecast.main.temp_min}
+              weather={forecast.weather[0].description}
+              weatherImg={forecast.weather[0].icon}
+            />
+          )
+        })}
+      </Space>
+    </div>
+
   )
 }
 
